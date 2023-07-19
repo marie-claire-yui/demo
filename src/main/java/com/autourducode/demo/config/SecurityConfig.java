669cb66130a7e4d86e1aadbb77dbf6f4c848ba7a
marie-claire-yui <marieclairekasai@gmail.com>
2023-07-19 16:53:34 +0200

    a

--- src/main/java/com/autourducode/demo/config/SecurityConfig.java
+++ src/main/java/com/autourducode/demo/config/SecurityConfig.java
@@ -3,19 +3,23 @@ package com.autourducode.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
+        // http.csrf((csrf) -> csrf.disable());
         http.authorizeHttpRequests((auth) -> auth
-            .antMatchers("/public").permitAll()
-            .anyRequest()
-            .authenticated()
+            // .antMatchers("/public").permitAll()
+            // .anyRequest()
+            // .authenticated()
+            .requestMatchers(new AntPathRequestMatcher("/produit/public")).permitAll().anyRequest().authenticated()
         )
-            .httpBasic();
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
